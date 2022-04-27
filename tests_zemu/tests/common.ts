11ea2d647ced4fa1a78d7258c3202c7e8e6becdd
Carlo Sala <carlosalag@protonmail.com>
2022-04-27 21:18:30 +0200

    remove blobs from common.ts

--- tests_zemu/tests/common.ts
+++ tests_zemu/tests/common.ts
@@ -13,7 +13,3 @@ export const models: DeviceModel[] = [
   { name: 'nanox', prefix: 'X', path: APP_PATH_X },
   { name: 'nanosp', prefix: 'SP', path: APP_PATH_SP },
 ]
-
-//    "name": "Balances_Transfer",
-export const txBasic =
-  '0a000087138f48a0f9e6e7904244eed35b3e3fca029b68bce55404bcd2ec177cd41f8233158139ae28a3dfaac5fe1560a5e9e05cd503000b63ce64c10c0500bc0200000500000048239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a'
