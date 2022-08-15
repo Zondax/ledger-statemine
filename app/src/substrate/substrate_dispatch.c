92b8239eaaca3386766228f3337df4d6b2c6f884
Carlo Sala <carlosalag@protonmail.com>
2022-08-15 21:21:59 +0200

    remove v6 support

--- app/src/substrate/substrate_dispatch.c
+++ app/src/substrate/substrate_dispatch.c
@@ -28,8 +28,6 @@ parser_error_t _readMethod(
     switch (c->tx_obj->transactionVersion) {
     case 7:
         return _readMethod_V7(c, moduleIdx, callIdx, &method->V7);
-    case 6:
-        return _readMethod_V6(c, moduleIdx, callIdx, &method->V6);
     default:
         return parser_tx_version_not_supported;
     }
@@ -40,8 +38,6 @@ uint8_t _getMethod_NumItems(uint32_t transactionVersion, uint8_t moduleIdx, uint
     switch (transactionVersion) {
     case 7:
         return _getMethod_NumItems_V7(moduleIdx, callIdx);
-    case 6:
-        return _getMethod_NumItems_V6(moduleIdx, callIdx);
     default:
         return parser_tx_version_not_supported;
     }
@@ -52,8 +48,6 @@ const char* _getMethod_ModuleName(uint32_t transactionVersion, uint8_t moduleIdx
     switch (transactionVersion) {
     case 7:
         return _getMethod_ModuleName_V7(moduleIdx);
-    case 6:
-        return _getMethod_ModuleName_V6(moduleIdx);
     default:
         return NULL;
     }
@@ -64,8 +58,6 @@ const char* _getMethod_Name(uint32_t transactionVersion, uint8_t moduleIdx, uint
     switch (transactionVersion) {
     case 7:
         return _getMethod_Name_V7(moduleIdx, callIdx);
-    case 6:
-        return _getMethod_Name_V6(moduleIdx, callIdx);
     default:
         return 0;
     }
@@ -76,8 +68,6 @@ const char* _getMethod_ItemName(uint32_t transactionVersion, uint8_t moduleIdx, 
     switch (transactionVersion) {
     case 7:
         return _getMethod_ItemName_V7(moduleIdx, callIdx, itemIdx);
-    case 6:
-        return _getMethod_ItemName_V6(moduleIdx, callIdx, itemIdx);
     default:
         return NULL;
     }
@@ -91,9 +81,6 @@ parser_error_t _getMethod_ItemValue(uint32_t transactionVersion, pd_Method_t* m,
     case 7:
         return _getMethod_ItemValue_V7(&m->V7, moduleIdx, callIdx, itemIdx, outValue,
             outValueLen, pageIdx, pageCount);
-    case 6:
-        return _getMethod_ItemValue_V6(&m->V6, moduleIdx, callIdx, itemIdx, outValue,
-            outValueLen, pageIdx, pageCount);
     default:
         return parser_tx_version_not_supported;
     }
@@ -104,8 +91,6 @@ bool _getMethod_ItemIsExpert(uint32_t transactionVersion, uint8_t moduleIdx, uin
     switch (transactionVersion) {
     case 7:
         return _getMethod_ItemIsExpert_V7(moduleIdx, callIdx, itemIdx);
-    case 6:
-        return _getMethod_ItemIsExpert_V6(moduleIdx, callIdx, itemIdx);
     default:
         return false;
     }
@@ -116,8 +101,6 @@ bool _getMethod_IsNestingSupported(uint32_t transactionVersion, uint8_t moduleId
     switch (transactionVersion) {
     case 7:
         return _getMethod_IsNestingSupported_V7(moduleIdx, callIdx);
-    case 6:
-        return _getMethod_IsNestingSupported_V6(moduleIdx, callIdx);
     default:
         return false;
     }
--- app/src/substrate/substrate_dispatch.h
+++ app/src/substrate/substrate_dispatch.h
@@ -21,7 +21,6 @@ extern "C" {
 
 #include "parser_common.h"
 #include "stdbool.h"
-#include "substrate_dispatch_V6.h"
 #include "substrate_dispatch_V7.h"
 #include <stddef.h>
 #include <stdint.h>
@@ -35,9 +34,6 @@ extern "C" {
                                                    \
         case 7:                                    \
             return PD_CALL_##CALL##_V7;            \
-                                                   \
-        case 6:                                    \
-            return PD_CALL_##CALL##_V6;            \
                                                    \
         default:                                   \
             return 0;                              \
